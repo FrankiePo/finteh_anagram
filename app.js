2532ef43907212759c91ef7c08dde2996bfbad82
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-03-03 19:44:39 +0300

    app: add prompt

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 /**
  * Created by frankiepo on 03/03/2017.
  */
+"use strict";
+const s1 = prompt("Enter first string");
+const s2 = prompt("Enter second string");
+
+if (s1.length != s2.length) alert(false);
